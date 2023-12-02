de2e79afd3d4b4701f2d3d6a0b2c0d6041c99b53
QueJobe <bqsithole22@live.com>
2023-12-02 10:33:39 -0800

    task 3 adding command line numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arguments = argv[1:]
+
+    #convert arguments to integers and calculate sum
+    total = sum(int(arg) for arg in arguments)
+
+    #print total
+    print(total)
