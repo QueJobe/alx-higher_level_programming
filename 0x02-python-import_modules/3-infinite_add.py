73071ea625528155ed50507541cf53214e815305
QueJobe <bqsithole22@live.com>
2023-12-02 10:36:04 -0800

    coreecting check 8

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     arguments = argv[1:]
-
     #convert arguments to integers and calculate sum
     total = sum(int(arg) for arg in arguments)
-
     #print total
     print(total)
