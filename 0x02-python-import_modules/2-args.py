141c2cb208182625bc6cf467305ea48dbb5da180
QueJobe <bqsithole22@live.com>
2023-12-02 06:41:34 -0800

    task 2 using commnd line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_arg = len(sys.argv) - 1
+    if num_arg == 0:
+        print("0 arguments.")
+    elif num_arg == 1:
+        print("1 argument: ")
+    else:
+        print("{} arguments:".format(num_arg))
+    for n in range(num_arg):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
