7e299219334258cafa4633c4c4507ec42b5143cb
QueJobe <bqsithole22@live.com>
2023-12-19 18:00:16 -0800

    task 3 division error

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        ans = a/b
+    except (ZeroDivisionError, FloatingPointError):
+        ans = None
+    finally:
+        print("Inside results: {}".format(ans))
+    return (ans)
