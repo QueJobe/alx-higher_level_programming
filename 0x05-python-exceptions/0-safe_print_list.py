3379d57c09525e08603de2415ccee98e1b7a7c55
QueJobe <bqsithole22@live.com>
2023-12-19 17:27:44 -0800

    task 0 of exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total_sum = 0
+
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total_sum += 1
+        except IndexError:
+            break
+    print()
+    return (total_sum)
