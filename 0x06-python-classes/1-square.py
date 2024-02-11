cdfe6fdaed8ef67b0ab7b6d0b825230c372c6e13
Blessing <bqsithole22@live.com>
2024-02-11 06:15:14 -0800

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of a square class"""
+
+
+class Square:
+    """Body of the class"""
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size(int): the size of the squre.
+        """
+        self.__size = size
