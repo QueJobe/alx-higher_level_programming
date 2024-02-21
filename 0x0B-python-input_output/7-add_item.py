1167b1bebcef0df1a61dd5aa208ac1096174f869
Blessing <bqsithole22@live.com>
2024-02-21 12:00:43 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,25 +5,14 @@ Moduile to load from and/or dump to
 a file
 """
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
-new_list_items = sys.argv[1:]
-items = []
-
-try:
-    file = open(filename, 'r')
-    content = file.read()
-    file.close()
-    if len(content):
-        items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-
-for item in new_list_items:
-    items.append(item)
-
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
