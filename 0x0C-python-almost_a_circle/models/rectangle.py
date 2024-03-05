f328945451c183a5f4d6b123577c7aa48be82ab2
Blessing <bqsithole22@live.com>
2024-03-05 06:40:45 -0800

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,8 @@ class Rectangle(Base):
         """Print rectangle with # symbol"""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height
+                )
