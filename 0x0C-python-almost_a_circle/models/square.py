52c6d472b639c0b071106b2eec4438f3aa6bd587
Blessing <bqsithole22@live.com>
2024-03-06 01:28:51 -0800

    fixing code style

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Body of the class square"""
-    def __init__(self, size,x=0, y=0, id=None):
+    def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
